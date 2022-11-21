2db9f5096d69375c7f7ab9aa6dab1b38cea3b6fb
Achal-mk <achalmottah22@gmail.com>
2022-11-21 15:19:59 +0530

    commit

---
+++ bin/variables2.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+void main(){
+  String name = "Anu";
+  int age = 22;
+  int phone = 7994093019;
+  double mark = 8.3;
+  String email = "anu777@gmail.com";
+
+  print("---my resume---");
+  print("my name is $name");
+  print("im $age years old");
+  print("my phone number is $phone");
+  print("i have got $mark in my graduation");
+  print("my gmail id is $email");
+}
